4257fcccc5c25b53924837d041ff034357a97478
DimitraKm <d.kontaki13@gmail.com>
2023-05-13 11:26:50 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace music
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            music a1 = new music("StrayKids", 32.38, "The sound");
+            music a2 = new music("KARD", 18.03, "Re");
+
+            Console.WriteLine(a1.length + a2.length);  
+
+        }
+    }
+}
---
+++ music.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace music
+{
+    internal class music
+    {
+        public string author;
+        public double length;
+        public string album_name;
+
+        public music(string author, double length, string album_name)
+        {
+            this.author = author;
+            this.length = length;
+            this.album_name = album_name;
+            Console.WriteLine(author + " " + length + " " + album_name);
+        }
+        public void print()
+        {
+            Console.WriteLine(author + " " + length + " " + album_name);
+        }
+       
+    }
+}
